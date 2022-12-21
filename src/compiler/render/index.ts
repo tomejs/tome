aaede3d3c42bf26124d251b7cdce796f28efa615
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2022-12-22 01:53:12 +0530

    Fixed a bug with wrong syntax for class method render

--- src/compiler/render/index.ts
+++ src/compiler/render/index.ts
@@ -2,7 +2,7 @@ import { NodeList } from "../parser/utils/types";
 import renderNode from "./render_node";
 
 export function render (template: NodeList): string {
-  let result = 'function render(root) {\n';
+  let result = 'render(root) {\n';
 
   template.forEach(node => {
     result += renderNode(node, 'root');
