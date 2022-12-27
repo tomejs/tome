05ae7e526432f3b3f76547eb656a98d9e7802183
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2022-12-27 16:24:24 +0530

    Some fixes in the component internal code

--- src/internal/component.ts
+++ src/internal/component.ts
@@ -1,12 +1,12 @@
 export default class Component {
   $$props: {[key: string]: any};
-  $$subs: {[key: string]: (() => void)[]} = {};
+  $$subs: {[key: string]: ((data: any) => void)[]} = {};
   $refs: {[key: string]: HTMLElement} = {};
   constructor(props: {[key: string]: any}) {
     this.$$props = props;
   }
 
-  $$sub(name: string | string[], fn: () => void) {
+  $$sub(name: string | string[], fn: (data: any) => void) {
     if(typeof name === 'string') {
       if(!this.$$subs[name]) {
         this.$$subs[name] = [];
@@ -17,8 +17,8 @@ export default class Component {
     }
   }
 
-  $$pub(name: string) {
-    this.$$subs[name] && this.$$subs[name].forEach(fn => fn());
+  $$pub(name: string, data: any) {
+    this.$$subs[name] && this.$$subs[name].forEach(fn => fn(data));
   }
 
   created() {}
@@ -34,13 +34,15 @@ export default class Component {
 
   update() {
     for(const key in this.$$subs) {
-      this.$$subs[key] && this.$$subs[key].forEach(fn => fn());
+      if(!key.startsWith('$$')) {
+        this.$$subs[key] && this.$$subs[key].forEach(fn => fn(undefined));
+      }
     }
   }
 
   setProps(props: {[key: string]: any}) {
     for(const key in props) {
-      this.$$props[key] = props[key];
+      (this as {[key: string] : any})[`$$${key}`] = props[key];
     }
   }
 
@@ -49,6 +51,6 @@ export default class Component {
   }
 
   $emit(event: string, ...args: any[]) {
-    this.$$pub(`$$${event}`, ...args);
+    this.$$pub.apply(this, [`$$${event}`, ...args]);
   }
 }
